40d4b75387ac22def09f80974577df68af925529
松下哲也 <matsushita@maxim-jp.net>
2020-10-21 17:40:39 +0900

    second commit

--- lineBot/index.php
+++ lineBot/index.php
@@ -2,11 +2,15 @@
     // Composerでインストールしたライブラリを一括読み込み
     require_once __DIR__ . '/vendor/autoload.php';
 
+    //POSTメソッドで渡される値を取得、表示
+    // $inputString = file_get_contents('php://input');
+    // error_log($inputString);
+
     // アクセストークンを使いCurlHTTPClientをインスタンス化
-    $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('Channel Access Token');
+    $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('Channel_Access_Token'));
 
     //CurlHTTPClientとシークレットを使いLINEBotをインスタンス化
-    $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'Channel Secret']);
+    $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('Channel_Secret')]);
 
     // LINE Messaging APIがリクエストに付与した署名を取得
     $signature = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
@@ -15,8 +19,29 @@
     $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
 
     foreach ($events as $event) {
-        // メッセージを返信
-        $response = $bot->replyMessage(
-            $event->getReplyToken(), new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($event->getText())  
-        );
+        //メッセージを返信
+        // $response = $bot->replyMessage(
+        //     $event->getReplyToken(), new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($event->getText())  
+        // );
+
+        // //テキストを返信
+        //$bot->replyText($event->getReplyToken(), 'Textmessage');
+        //テキストを返信し次のイベント処理へ
+        replyTextMessage($bot, $event->getReplyToken(), 'TextMessage');
     }
+
+        //テキストを返信。引数はLINEbot、返信先、テキスト
+        function replyTextMessage($bot, $replyToken, $text){
+            //返信を行いレスポンスを取得
+            //TextMessageBuilderの引数はテキスト
+            $response = $bot->replyMessage($replyToken, new LINE\LINEBot\MessageBuilder\TextMessageBuilder($text));
+
+            //レスポンスが異常な場合
+            if (!$response->isSucceeded()){
+                //エラー内容を出力
+                error_log('Failed! '. $response->getHTTPStatus .' ' . $response->getRawBOdy());
+            }
+        }
+
+?>
+    
