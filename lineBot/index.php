9b86def0ef6fa0a3d15843495f855746265f7e49
松下哲也 <matsushita@maxim-jp.net>
2020-10-26 14:00:01 +0900

    メッセージを返信する関数定義

--- lineBot/index.php
+++ lineBot/index.php
@@ -49,7 +49,7 @@ foreach ($events as $event) {
         new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('TextMessage'),
         new \LINE\LINEBot\MessageBuilder\ImageMessageBuilder('https://' . $_SERVER['HTTP_HOST'] . '/imgs/original.jpg', 'https://' . $_SERVER['HTTP_HOST'] . '/imgs/view.jpg'),
         new \LINE\LINEBot\MessageBuilder\LocationMessageBuilder('LINE', '東京都渋谷区渋谷2-21-1 ヒカリエ27階', 35.659025, 139.703473),
-        new \LINE\LINEBot\MessageBuilder\MultiMessageBuilder(1, 1)
+        new \LINE\LINEBot\MessageBuilder\StickerMessageBuilder(1, 1)
     );
 }
 
