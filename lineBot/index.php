a79f96bbe1a643bc8eea8d4a0bf927b505506d23
松下哲也 <matsushita@maxim-jp.net>
2020-10-26 13:37:26 +0900

    スタンプを返信する関数定義

--- lineBot/index.php
+++ lineBot/index.php
@@ -39,7 +39,10 @@ foreach ($events as $event) {
     // image($bot, $event->getReplyToken(), 'https://' . $_SERVER['HTTP_HOST'] . '/imgs/original.jpg');
 
     //位置情報の返信
-    loca($bot, $event->getReplyToken(), 'LINE', '東京都渋谷区渋谷2-21-1 ', 35.659025, 139.703473);
+    // loca($bot, $event->getReplyToken(), 'LINE', '東京都渋谷区渋谷2-21-1 ', 35.659025, 139.703473);
+
+    //スタンプを返信
+    sticker($bot, $event->getReplyToken(), 1, 1);
 }
 
 //’TextMessage’を返す関数
@@ -82,7 +85,16 @@ function image($bot, $replyToken, $originalImageUrl, $viewImageUrl)
 function loca($bot, $replyToken, $title, $address, $lat, $lon)
 {
     $response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\LocationMessageBuilder($title, $address, $lat, $lon));
+    if (!$response->isSucceeded())
+        {
+            error_log('Failed! '. $response->getHTTPStatus . ' ' . $response->getRawBody());
+        }    
+}
 
+//スタンプを返す関数
+function sticker($bot, $replyToken, $packageID, $stickerID)
+{
+    $response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\StickerMessageBuilder($packageID, $stickerID));
     if (!$response->isSucceeded())
         {
             error_log('Failed! '. $response->getHTTPStatus . ' ' . $response->getRawBody());
